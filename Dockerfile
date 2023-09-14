37c82afb8c3112be1df26c5cda6fc6eaf7a9ed46
lightsing <light.tsing@gmail.com>
2023-09-14 16:40:13 +0800

    add worker supervisor

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM scrolltech/go-rust-builder:go-1.19-rust-nightly-2022-12-10 as builder
+
+COPY worker /worker
+RUN cd /worker && cargo build --release
+COPY mock-runner /mock-runner
+RUN cd /mock-runner && \
+    cargo build --release --bin mock-runner && \
+    cp `find ./target/release/ | grep libzktrie.so` /usr/lib/ \
+
+FROM ubuntu:20.04
+
+ENV RUST_BACKTRACE=1
+ENV RUST_LOG=info
+ENV RUNNER_PATH=/usr/local/bin/mock-runner
+COPY --from=builder /worker/target/release/worker /usr/local/bin/
+COPY --from=builder /target/release/mock-runner /usr/local/bin/
+COPY --from=builder /usr/lib/libzktrie.so /usr/lib/
+
+ENTRYPOINT ["/usr/local/bin/worker"]
---
+++ worker/src/main.rs
@@ -0,0 +1,112 @@
+#[macro_use]
+extern crate tracing;
+
+use std::env::var;
+use std::fs;
+use std::path::{Path, PathBuf};
+use tracing_subscriber::EnvFilter;
+
+fn main() {
+    let worker_index: usize = var("WORKER_INDEX")
+        .map(|s| s.parse().unwrap())
+        .expect("WORKER_INDEX not set");
+    let output_path: PathBuf = var("OUTPUT_PATH")
+        .map(PathBuf::from)
+        .expect("OUTPUT_PATH not set");
+    fs::create_dir_all(output_path.as_path()).expect("cannot create output dir");
+
+    let appender = tracing_appender::rolling::never(
+        output_path.as_path(),
+        format!("worker-{}.log", worker_index),
+    );
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::builder().from_env_lossy())
+        .with_writer(appender)
+        .init();
+    info!("output path: {}", output_path.display());
+
+    let runner_path: PathBuf = var("RUNNER_PATH")
+        .map(PathBuf::from)
+        .map_err(|e| {
+            error!("RUNNER_PATH not set: {:?}", e);
+            e
+        })
+        .unwrap();
+    if !runner_path.is_file() {
+        error!("runner binary not found at: {}", runner_path.display());
+        std::process::exit(1);
+    }
+    let traces_path: PathBuf = var("TRACES_PATH")
+        .map(PathBuf::from)
+        .map_err(|e| {
+            error!("TRACES_PATH not set: {:?}", e);
+            e
+        })
+        .unwrap();
+    info!("loading traces from: {}", traces_path.display());
+
+    let total_workers: usize = var("TOTAL_WORKERS")
+        .map(|s| s.parse().unwrap())
+        .map_err(|e| {
+            error!("TOTAL_WORKERS not set: {:?}", e);
+            e
+        })
+        .unwrap();
+    info!("total workers: {total_workers}, worker index: {worker_index}");
+
+    let all_jobs = load_jobs(traces_path.as_path());
+    info!("total jobs: {}", all_jobs.len());
+    let job_per_worker = (all_jobs.len() + total_workers - 1) / total_workers;
+    info!("jobs per worker: {job_per_worker}");
+    let worker_jobs = all_jobs
+        .chunks(job_per_worker)
+        .skip(worker_index)
+        .next()
+        .expect("impossible")
+        .to_vec();
+    info!("loaded {} jobs to run", worker_jobs.len());
+
+    for (idx, job) in worker_jobs.into_iter().enumerate() {
+        let idx = idx + worker_index * job_per_worker;
+        let trace_name = job
+            .file_stem()
+            .expect("cannot get file name")
+            .to_string_lossy();
+        let output_file = output_path.join(format!("{}-result.json", trace_name));
+        info!("running job {}/{}: {}", idx + 1, all_jobs.len(), trace_name);
+        let task = std::process::Command::new(runner_path.as_path())
+            .arg(job.as_path())
+            .arg("--output")
+            .arg(output_file.as_path())
+            .stdout(std::process::Stdio::piped())
+            .stderr(std::process::Stdio::piped())
+            .spawn()
+            .and_then(|mut child| child.wait());
+        match task {
+            Err(e) => {
+                error!("failed to exec task {trace_name}: {:?}", e);
+            }
+            Ok(result) => {
+                if !result.success() {
+                    error!("task {trace_name} failed: {:?}", result);
+                } else {
+                    info!("task {trace_name} finished");
+                }
+            }
+        }
+    }
+}
+
+fn load_jobs(traces_path: &Path) -> Vec<PathBuf> {
+    let mut jobs = Vec::new();
+    for entry in fs::read_dir(traces_path).expect("could not read traces") {
+        let entry = entry.expect("could not resolve entry");
+        let path = entry.path();
+        if path.is_dir() {
+            jobs.extend(load_jobs(path.as_path()));
+        } else if path.is_file() && path.extension().unwrap() == "json" {
+            jobs.push(path);
+        }
+    }
+    jobs
+}
